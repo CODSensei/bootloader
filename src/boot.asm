fbe42daece0abbd8337e876f68f40dcfb15059ff
CODSensei <krishna2342005@gmail.com>
2024-12-25 00:01:17 +0530

    completed part2 ie made bootloader ready for kernal;

--- src/boot.asm
+++ src/boot.asm
@@ -1,6 +1,10 @@
 [BITS 16]
 [ORG 0x7c00]
 
+
+CODE_OFFSET epu 0x8
+DATA_OFFSET equ 0x10
+
 start:
     cli ;Clear Interrupts
     mov ax, 0x00
@@ -9,21 +13,63 @@ start:
     mov ss, ax
     mov sp, 0x7c00
     sti ;enable Interrupts
-    mov si, msg
 
-print: 
-    lodsb ;load byte at ds:si to AL register and increment sti
-    cmp al, 0
-    je done
-    mov ah, 0x0E
-    int 0x10
-    jmp print
-
-done: 
+load_PM:
     cli
-    hlt ;stop cpu execution
+    lgdt(gdt_descriptor)
+    mov eax, cr0
+    or al, 1
+    mov cr0, eax
+    jmp CODE_OFFSET:PModeMain
 
-msg: db 'Hello World', 0
+
+;GDT Implementation
+
+gdt_start:
+    dd 0x0
+    dd 0x0
+
+    ;code segment descriptor
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 10011010b
+    db 11001111b
+    db 0x00
+
+    ;data segment descriptor
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 10010010b
+    db 11001111b
+    db 0x00
+
+gdt_end:
+    
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1 
+    dd gdt_start
+
+[BITS 32]
+
+PModeMain:
+    mov ax, DATA_OFFSET
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov ss, ax
+    mov gs, ax
+    mov ebp, 0x9C00
+    mov esp, ebp
+
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+
+    jmp $
+
 
 times 510 - ($ - $$) db 0  
 
